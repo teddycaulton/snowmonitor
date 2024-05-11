bdc841b467d065a7dd058a5f67652248db6905a8
Teddy Caulton <teddycaulton@live.com>
2024-05-10 18:50:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,16 +13,20 @@ st.header("Current Alerts")
 alert_data = helpers.read_current_alerts(session)
 for row in alert_data.values:
     col1, col2 = st.columns(2)
+    ### use expander function
+    with col1:
+        st.write
     with col1:
         st.write(f"Alert Name: {row[0]}")
         st.write(f"Condition: {row[6]}")
         st.write(f"Action: {row[7]}")
         st.write(f"Owner: {row[3]}")
     with col2:
-        st.write(f"Database: {row[1]}")
-        st.write(f"Schema: {row[2]}")
-        st.write(f"Schedule: {row[4]}")
-        st.write(f"State: {row[5]}")
+        if st.checkbox("More Info"):
+            st.write(f"Database: {row[1]}")
+            st.write(f"Schema: {row[2]}")
+            st.write(f"Schedule: {row[4]}")
+            st.write(f"State: {row[5]}")
     if st.checkbox("Run History"):
         st.write("Run History Here")
     st.markdown("""---""")
