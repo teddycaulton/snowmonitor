39aa6b1fa4bd431a268cb7828006ebb8c6816c2d
Teddy Caulton <118858063+teddycaultonhakkoda@users.noreply.github.com>
2024-07-10 17:20:53 -0700

    added features for resuming/suspending alerts and adding alerts

--- 0_Alert_Management.py
+++ 0_Alert_Management.py
@@ -11,9 +11,10 @@ st.set_page_config(
 
 session = helpers.create_snowpark_session('tcaulton', 'Foo1234!', 'QTB38119', 'accountadmin', 'compute_wh')
 
-st.title("Snowmonitor: Snowflake Alert Management")
+st.title(":blue[SnowMonitor] 🔎")
+st.header("UI Driven Snowflake Alert Management")
 
-st.header("Current Alerts")
+st.subheader('Current Alerts', divider='rainbow')
 alert_data = helpers.read_current_alerts(session)
 print(alert_data)
 for row in alert_data.values:
@@ -33,8 +34,21 @@ for row in alert_data.values:
         if st.checkbox("Run History (last 10 runs)", key = row[0]):
             alert_history_df = helpers.read_alert_history(session, row[0])
             st.table(alert_history_df)
-        st.error("Be sure you want to select the button below")
-        if st.button("Delete Alert", key = f'{row[0]}_delete_alert'):
-            helpers.execute_sql(session, f"DROP ALERT IF EXISTS {row[1]}.{row[2]}.{row[0]}")
-
-
+        st.error("Be sure you want to select the buttons below")
+        button1, button2, button3 = st.columns(3)
+        with button1:
+            if st.button("Delete Alert", key = f'{row[0]}_delete_alert'):
+                helpers.execute_sql(session, f"DROP ALERT IF EXISTS {row[1]}.{row[2]}.{row[0]}")
+        with button2:
+            if row[5] == 'suspended':
+                if st.button("Resume Alert", key = f'{row[0]}_resume_alert'):
+                    helpers.execute_sql(session, f"ALTER ALERT {row[1]}.{row[2]}.{row[0]} RESUME")
+                    st.rerun()
+            elif row[5] == 'started':
+                if st.button("Suspend Alert", key = f'{row[0]}_suspend_alert'):
+                    helpers.execute_sql(session, f"ALTER ALERT {row[1]}.{row[2]}.{row[0]} SUSPEND")
+                    st.rerun()
+        with button3:
+            if row[5] == 'started':
+                if st.button("Execute Alert", key = f'{row[0]}_execute_alert'):
+                    helpers.execute_sql(session, f"EXECUTE ALERT {row[1]}.{row[2]}.{row[0]}")
